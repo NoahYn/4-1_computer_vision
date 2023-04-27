355b8cdedc5fde36ebd22fbcecf8b4a6b9d3ed26
noah <101003842+NoahYn@users.noreply.github.com>
2023-04-27 23:57:58 +0900

    submit

--- 2021202033_윤성민_ver2/ImageRestoration.c
+++ 2021202033_윤성민_ver2/ImageRestoration.c
@@ -6,21 +6,21 @@
 
 #pragma warning(disable:4996)
 
-#define W 512
-#define H 512
+#define W 512 // width of image
+#define H 512 // height of image
 
-int cmp(const unsigned char* a, const unsigned char* b) {
-	return ((*a > *b) ? 1 : -1);
+int cmp(const unsigned char* a, const unsigned char* b) { // compare function for quick sort
+	return ((*a > *b) ? 1 : -1); 
 }
 
-void getGaussFilter(double(*filter)[5], int size, double std) {
+void getGaussFilter(double(*filter)[5], int size, double std) { // size = window size, std = standard deviation
 	double sum = 0;
 	double dist = (size - 1) / 2.0; // distance from origin to edge
 
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++) {
-			double x = i - dist, y = j - dist;
-			filter[i][j] = exp(-(x * x + y * y) / (2 * std * std));
+			double x = i - dist, y = j - dist; 
+			filter[i][j] = exp(-(x * x + y * y) / (2 * std * std)); // gaussian filter formular
 			sum += filter[i][j];
 		}
 	}
@@ -62,8 +62,8 @@ int main() {
 	unsigned char origin[H][W];
 	unsigned char gaussian[H][W];
 	unsigned char saltpepper[H][W];
-	double filter[5][5];
-	unsigned char filter2[25];
+	double filter[5][5]; // for gaussian filter
+	unsigned char filter2[25]; // for median and alpha-trimmed mean filter
 
 	/* read image from lena.raw */
 	for (int i = 0; i < H; i++) {
@@ -83,7 +83,7 @@ int main() {
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
 			char gaussnoise = getGaussRand(0, 10);
-			gaussian[i][j] = origin[i][j] + gaussnoise;
+			gaussian[i][j] = origin[i][j] + gaussnoise; // add noise
 			fputc(gaussian[i][j], AGN);
 			SSE += gaussnoise * gaussnoise;
 		}
@@ -97,7 +97,7 @@ int main() {
 	for (int i = 0; i < W * H * 0.3; i++) {
 		int randi = rand() % H;
 		int randj = rand() % W;
-		saltpepper[randi][randj] = (rand() % 2) * 255;
+		saltpepper[randi][randj] = (rand() % 2) * 255; // 0 or 255
 	}
 
 	// write salt & pepper image and print PSNR
@@ -136,15 +136,15 @@ int main() {
 	getGaussFilter(filter, 3, 0.7);
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
-			double sum = 0;
+			double conv = 0;
 			for (int dir = 0; dir < 9; dir++) {
 				int x = i + dx3[dir] - 1;
 				int y = j + dy3[dir] - 1;
 				if (x < 0 || x >= H || y < 0 || y >= W) continue; // zero padding : nothing to add
-				sum += gaussian[x][y] * filter[dx3[dir]][dy3[dir]];
+				conv += gaussian[x][y] * filter[dx3[dir]][dy3[dir]]; // convolution 
 			}
-			fputc((unsigned char)sum, GG3);
-			double diff = origin[i][j] - (unsigned char)sum;
+			fputc((unsigned char)conv, GG3);
+			double diff = origin[i][j] - (unsigned char)conv;
 			SSE += diff * diff;
 		}
 	}
@@ -157,15 +157,15 @@ int main() {
 	getGaussFilter(filter, 3, 10);
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
-			double sum = 0;
+			double conv = 0;
 			for (int dir = 0; dir < 9; dir++) {
 				int x = i + dx3[dir] - 1;
 				int y = j + dy3[dir] - 1;
 				if (x < 0 || x >= H || y < 0 || y >= W) continue; // zero padding : nothing to add
-				sum += saltpepper[x][y] * filter[dx3[dir]][dy3[dir]];
+				conv += saltpepper[x][y] * filter[dx3[dir]][dy3[dir]];
 			}
-			fputc((unsigned char)sum, SG3);
-			double diff = origin[i][j] - (unsigned char)sum;
+			fputc((unsigned char)conv, SG3);
+			double diff = origin[i][j] - (unsigned char)conv;
 			SSE += diff * diff;
 		}
 	}
@@ -180,15 +180,15 @@ int main() {
 	getGaussFilter(filter, 5, 0.7);
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
-			double sum = 0;
+			double conv = 0;
 			for (int dir = 0; dir < 25; dir++) {
 				int x = i + dx5[dir] - 2;
 				int y = j + dy5[dir] - 2;
 				if (x < 0 || x >= H || y < 0 || y >= W) continue; // zero padding : nothing to add
-				sum += gaussian[x][y] * filter[dx5[dir]][dy5[dir]];
+				conv += gaussian[x][y] * filter[dx5[dir]][dy5[dir]];
 			}
-			fputc((unsigned char)sum, GG5);
-			double diff = origin[i][j] - (unsigned char)sum;
+			fputc((unsigned char)conv, GG5);
+			double diff = origin[i][j] - (unsigned char)conv;
 			SSE += diff * diff;
 		}
 	}
@@ -201,15 +201,15 @@ int main() {
 	getGaussFilter(filter, 5, 10);
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
-			double sum = 0;
+			double conv = 0;
 			for (int dir = 0; dir < 9; dir++) {
 				int x = i + dx5[dir] - 2;
 				int y = j + dy5[dir] - 2;
 				if (x < 0 || x >= H || y < 0 || y >= W) continue; // zero padding : nothing to add
-				sum += saltpepper[x][y] * filter[dx5[dir]][dy5[dir]];
+				conv += saltpepper[x][y] * filter[dx5[dir]][dy5[dir]];
 			}
-			fputc((unsigned char)sum, SG5);
-			double diff = origin[i][j] - (unsigned char)sum;
+			fputc((unsigned char)conv, SG5);
+			double diff = origin[i][j] - (unsigned char)conv;
 			SSE += diff * diff;
 		}
 	}
@@ -231,8 +231,8 @@ int main() {
 				if (x < 0 || x >= H || y < 0 || y >= W) filter2[dir] = 0; // zero padding
 				else filter2[dir] = gaussian[x][y];
 			}
-			qsort(filter2, 9, sizeof(unsigned char), cmp);
-			fputc(filter2[4], GM3);
+			qsort(filter2, 9, sizeof(unsigned char), cmp); // sorting
+			fputc(filter2[4], GM3); // median
 			double diff = origin[i][j] - filter2[4];
 			SSE += diff * diff;
 		}
@@ -323,8 +323,8 @@ int main() {
 				}
 			}
 			qsort(filter2, 9, sizeof(unsigned char), cmp);
-			sum -= (filter2[0] + filter2[8]);
-			sum /= 7;
+			sum -= (filter2[0] + filter2[8]); // alpha-trim
+			sum /= 7; // mean
 			fputc((unsigned char)sum, GA23);
 			double diff = origin[i][j] - sum;
 			SSE += diff * diff;
@@ -336,6 +336,7 @@ int main() {
 
 	/* 3x3 0.2 Alpha-trimmed mean filtering -> Salt & pepper noise */
 	// alpha = 1 (floor of 0.2 * 3 * 3) 
+	SSE = 0;
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
 			float sum = 0;
@@ -393,6 +394,7 @@ int main() {
 	printf("%.4f\t\t|\t\t", PSNR);
 
 	/* 3x3 0.4 Alpha-trimmed mean filtering -> Salt & pepper noise */
+	SSE = 0;
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
 			float sum = 0;
@@ -419,28 +421,30 @@ int main() {
 	PSNR = 10 * log10(255 * 255 / MSE);
 	printf("%.4f\n", PSNR);
 
-
-
-
-
-
-
-
-
 	/* 5x5 0.1 Alpha-trimmed mean filtering -> gaussian noise image */
 	printf("----------------------------------------------------------------------------\n");
 	printf("After Alpha 0.1 5x5\t\t");
 	SSE = 0;
+	alpha = 2; // 5 * 5 * 0.1
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
+			float sum = 0;
 			for (int dir = 0; dir < 25; dir++) {
 				int x = i + dx5[dir] - 2;
 				int y = j + dy5[dir] - 2;
-				if (x < 0 || x >= H || y < 0 || y >= W) filter2[dir] = 0; // zero padding : nothing to add
-				filter2[dir] = saltpepper[x][y];
+				if (x < 0 || x >= H || y < 0 || y >= W) filter2[dir] = 0; // zero padding 
+				else {
+					filter2[dir] = gaussian[x][y];
+					sum += filter2[dir];
+				}
 			}
 			qsort(filter2, 25, sizeof(unsigned char), cmp);
-			double diff = origin[i][j] - filter2[12];
+			for (int i = 0; i < alpha; i++) {
+				sum -= (filter2[i] + filter2[24 - i]);
+			}
+			sum /= 21;
+			fputc((unsigned char)sum, GA15);
+			double diff = origin[i][j] - sum;
 			SSE += diff * diff;
 		}
 	}
@@ -452,14 +456,23 @@ int main() {
 	SSE = 0;
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
+			float sum = 0;
 			for (int dir = 0; dir < 25; dir++) {
 				int x = i + dx5[dir] - 2;
 				int y = j + dy5[dir] - 2;
-				if (x < 0 || x >= H || y < 0 || y >= W) filter2[dir] = 0; // zero padding : nothing to add
-				filter2[dir] = saltpepper[x][y];
+				if (x < 0 || x >= H || y < 0 || y >= W) filter2[dir] = 0; // zero padding 
+				else {
+					filter2[dir] = saltpepper[x][y];
+					sum += filter2[dir];
+				}
 			}
 			qsort(filter2, 25, sizeof(unsigned char), cmp);
-			double diff = origin[i][j] - filter2[12];
+			for (int i = 0; i < alpha; i++) {
+				sum -= (filter2[i] + filter2[24 - i]);
+			}
+			sum /= 21;
+			fputc((unsigned char)sum, SA15);
+			double diff = origin[i][j] - sum;
 			SSE += diff * diff;
 		}
 	}
@@ -471,16 +484,26 @@ int main() {
 	printf("----------------------------------------------------------------------------\n");
 	printf("After Alpha 0.4 5x5\t\t");
 	SSE = 0;
+	alpha = 10; // 5 * 5 * 0.4
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
+			float sum = 0;
 			for (int dir = 0; dir < 25; dir++) {
 				int x = i + dx5[dir] - 2;
 				int y = j + dy5[dir] - 2;
-				if (x < 0 || x >= H || y < 0 || y >= W) filter2[dir] = 0; // zero padding : nothing to add
-				filter2[dir] = saltpepper[x][y];
+				if (x < 0 || x >= H || y < 0 || y >= W) filter2[dir] = 0; // zero padding 
+				else {
+					filter2[dir] = gaussian[x][y];
+					sum += filter2[dir];
+				}
 			}
 			qsort(filter2, 25, sizeof(unsigned char), cmp);
-			double diff = origin[i][j] - filter2[12];
+			for (int i = 0; i < alpha; i++) {
+				sum -= (filter2[i] + filter2[24 - i]);
+			}
+			sum /= 5;
+			fputc((unsigned char)sum, GA45);
+			double diff = origin[i][j] - sum;
 			SSE += diff * diff;
 		}
 	}
@@ -492,14 +515,23 @@ int main() {
 	SSE = 0;
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
+			float sum = 0;
 			for (int dir = 0; dir < 25; dir++) {
 				int x = i + dx5[dir] - 2;
 				int y = j + dy5[dir] - 2;
-				if (x < 0 || x >= H || y < 0 || y >= W) filter2[dir] = 0; // zero padding : nothing to add
-				filter2[dir] = saltpepper[x][y];
+				if (x < 0 || x >= H || y < 0 || y >= W) filter2[dir] = 0; // zero padding 
+				else {
+					filter2[dir] = saltpepper[x][y];
+					sum += filter2[dir];
+				}
 			}
 			qsort(filter2, 25, sizeof(unsigned char), cmp);
-			double diff = origin[i][j] - filter2[12];
+			for (int i = 0; i < alpha; i++) {
+				sum -= (filter2[i] + filter2[24 - i]);
+			}
+			sum /= 5;
+			fputc((unsigned char)sum, SA45);
+			double diff = origin[i][j] - sum;
 			SSE += diff * diff;
 		}
 	}
@@ -507,8 +539,6 @@ int main() {
 	PSNR = 10 * log10(255 * 255 / MSE);
 	printf("%.4f\n", PSNR);
 
-
-	
 	fclose(LENA);
 	fclose(AGN); fclose(SPN);
 	fclose(GG3); fclose(GG5); fclose(GM3); fclose(GM5); fclose(GA23); fclose(GA43); fclose(GA15); fclose(GA45);
